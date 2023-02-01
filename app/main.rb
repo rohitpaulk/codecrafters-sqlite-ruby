fb0b39be23b9f690050ca68efd24c9bba3c5b971
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-02-01 13:30:17 +0000

    pass stage 1

--- app/main.rb
+++ app/main.rb
@@ -6,9 +6,8 @@ if command == ".dbinfo"
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts "Logs from your program will appear here"
 
-    # Uncomment this to pass the first stage
-    # database_file.seek(16)  # Skip the first 16 bytes of the header
-    # page_size = database_file.read(2).unpack("n")[0]
-    # puts "database page size: #{page_size}"
+    database_file.seek(16) # Skip the first 16 bytes of the header
+    page_size = database_file.read(2).unpack1("n")
+    puts "database page size: #{page_size}"
   end
 end
